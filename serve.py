088c19b73f5c62807b07f9ebce44bf35ad3f4b6b
SUBCOLL10 <subcoll10@MacStudio-von-SUBCOLL10.local>
2026-06-24 14:23:05 +0200

    Initial commit — application de facturation

---
+++ serve.py
@@ -0,0 +1,9 @@
+import os, http.server, socketserver
+os.chdir("/Users/paulomeireles/Documents/Substances/00-ADMIN/01-FACTURATION/99_applucation facturation in progress")
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def log_message(self, *a): pass
+    def do_GET(self):
+        if self.path == '/': self.path = '/Facturation.html'
+        return super().do_GET()
+with socketserver.TCPServer(("", 7654), Handler) as httpd:
+    httpd.serve_forever()
